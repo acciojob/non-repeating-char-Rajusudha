7e6dd1c0625cbe3de5bace5c3f89df645691d1c8
Raju Kumar <129931026+Rajusudha@users.noreply.github.com>
2024-02-13 08:38:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let count=0;
+	let s="";
+	for (let i=0;i<str.length();i++) {
+		for (let j = 1; j < str.length()-1; j++) {
+			if(str.charAt(i)==charAt(j)){
+				count++;
+			}
+			s=str.charAt(i);
+			
+		}
+		
+	}
+	return s;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
